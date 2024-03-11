7e41681d75e0ede77c3d9437ec284e7d150c0319
rushil23451 <162873099+rushil23451@users.noreply.github.com>
2024-03-11 23:02:53 +0530

    Update Assembler.py

--- files/Assembler.py
+++ files/Assembler.py
@@ -1,5 +1,21 @@
 #Assembler made in python
 # write functions for all types here
+def sextRISB(n):
+   binary_string = bin(n & ((1 << 12) - 1))[2:].zfill(12)
+   return binary_string
+
+def sextJ(n):
+    binary_string = bin(n & ((1 << 21) - 1))[2:].zfill(21)
+    return binary_string
+
+def sextU(n):
+    binary_string = bin(n & ((1 << 20) - 1))[2:].zfill(20)
+    return binary_string
+
+def sextB(n):
+    binary_string = bin(n & ((1 << 13) - 1))[2:].zfill(13)
+    return binary_string
+
 def r_type(regi_dict,function3_r,function7_r,instruction,r_instruct):
     return function7_r[instruction[0]]+regi_dict[instruction[3]]+regi_dict[instruction[2]]+function3_r[instruction[0]]+regi_dict[instruction[1]]+r_instruct[instruction[0]]
 def utype(instruction,regi_dict,u_instruct):

acf7f4dd844f1c80f5b42af8a5a7c26ec4e9b4c9
MrPanda6704 <utkarsh23570@iiitd.ac.in>
2024-03-11 22:58:57 +0530

    Update jtype.py

--- files/jtype.py
+++ files/jtype.py
@@ -1,4 +1,4 @@
 def jtype(instruction):
     register = instruction[1::]
     t=sextUJ(int(register[1]))
-    return t[0]+t[9:20]+t[9]+t[1:8]+registers[register[0]]+"0010111"
+    return t[0]+t[9:20]+t[9]+t[1:8]+regi_dict[register[0]]+"0010111"

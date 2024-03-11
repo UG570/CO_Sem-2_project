3d5492bdaebda9ca915c6ebf5cdff5191edd5063
rushil23451 <162873099+rushil23451@users.noreply.github.com>
2024-03-11 23:01:35 +0530

    Update Assembler.py

--- files/Assembler.py
+++ files/Assembler.py
@@ -2,3 +2,7 @@
 # write functions for all types here
 def r_type(regi_dict,function3_r,function7_r,instruction,r_instruct):
     return function7_r[instruction[0]]+regi_dict[instruction[3]]+regi_dict[instruction[2]]+function3_r[instruction[0]]+regi_dict[instruction[1]]+r_instruct[instruction[0]]
+def utype(instruction,regi_dict,u_instruct):
+    opcode=u_instruct[instruction[0]]
+    t=sextU(int(instruction[2]))
+    return t+regi_dict[instruction[1]+opcode

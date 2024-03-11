fee052ba452f859dbc86710c2d80fb65f21b85f8
rushil23451 <162873099+rushil23451@users.noreply.github.com>
2024-03-11 22:51:32 +0530

    Update Assembler.py

--- files/Assembler.py
+++ files/Assembler.py
@@ -1,2 +1,4 @@
 #Assembler made in python
 # write functions for all types here
+def r_type(regi_dict,function3_r,function7_r,instruction,r_instruct):
+    return function7_r[instruction[0]]+regi_dict[instruction[3]]+regi_dict[instruction[2]]+function3_r[instruction[0]]+regi_dict[instruction[1]]+r_instruct[instruction[0]]

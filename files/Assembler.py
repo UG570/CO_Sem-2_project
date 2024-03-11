b95ab60750b6afd8663be256f75b6588058c3d6b
MrPanda6704 <utkarsh23570@iiitd.ac.in>
2024-03-11 23:26:44 +0530

    updated driver code

--- files/Assembler.py
+++ files/Assembler.py
@@ -18,7 +18,7 @@ def sextB(n):
 
 def r_type(regi_dict,function3_r,function7_r,instruction,r_instruct):
     return function7_r[instruction[0]]+regi_dict[instruction[3]]+regi_dict[instruction[2]]+function3_r[instruction[0]]+regi_dict[instruction[1]]+r_instruct[instruction[0]]
-def utype(instruction,regi_dict,u_instruct):
+def u_type(instruction,regi_dict,u_instruct):
     opcode=u_instruct[instruction[0]]
     t=sextU(int(instruction[2]))
     return t+regi_dict[instruction[1]]+opcode
@@ -33,7 +33,7 @@ def i_type(instruction, regi_dict,function3_i,i_instruct):
     t = sextRISB(int(instruction[3]))
     return t + regi_dict[instruction[2]] + function3_i[instruction[0]] + regi_dict[instruction[1]]+i_instruct[instruction[0]]
 
-def jtype(instruction):
+def j_type(instruction):
     register = instruction[1::]
     t=sextJ(int(register[1]))
     return t[0]+t[9:20]+t[9]+t[1:8]+regi_dict[register[0]]+"0010111"

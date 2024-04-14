f09eb99ae0997b9df2fbd7599db975b6149630a0
simar23527 <162867408+simar23527@users.noreply.github.com>
2024-04-14 19:58:35 +0530

    Updated_instrcutions input.py

--- CO Project evaluation framework Apr2/SimpleSimulator/input.py
+++ CO Project evaluation framework Apr2/SimpleSimulator/input.py
@@ -23,6 +23,56 @@ def r_type_splitting(str):
     r_type_implementation(instruction ,str[-12:-7:1], str[-20:-15:1], str[-25:-20:1])
 
 
+def i_type_splitting(str):
+    # global instruction
+    if str[-15:-12:1]== "000":
+        if str[0:7]== "0010011":
+            instruction="addi"
+        else:
+            instruction = "jalr"
+    elif str[-15:-12:1]== "010":
+        instruction = "lw"
+    elif str[-15:-12:1]== "011":
+        instruction = "sltiu"
+    i_type_implementation(instruction ,str[-12:-7:1], str[-20:-15:1], str[-25:-20:1])
+
+
+def s_type_splitting(str):
+    # global instruction
+    if str[-15:-12:1]== "010":
+            instruction="sw"
+    s_type_implementation(instruction ,str[-12:-7:1], str[-20:-15:1], str[-25:-20:1])
+
+
+def b_type_splitting(str):
+    # global instruction
+    if str[-15:-12:1]== "000":
+            instruction="beq"
+    elif str[-15:-12:1]== "001":
+        instruction = "bne"
+    elif str[-15:-12:1]== "100":
+        instruction = "blt"
+    elif str[-15:-12:1]== "101":
+        instruction = "bge"
+    elif str[-15:-12:1]== "110":
+        instruction = "bltu"
+    elif str[-15:-12:1]== "111":
+        instruction = "bgeu"
+    b_type_implementation(instruction ,str[-12:-7:1], str[-20:-15:1], str[-25:-20:1])
+
+
+def u_type_instruction(str):
+  if str[0:7]=="0110111":
+    instruction="lui"
+  else:
+    instruction="auipc"
+  u_type_implementation(instruction ,str[-12:-7:1], str[-20:-15:1], str[-25:-20:1])
+
+
+def j_type_instruction(str):
+  if str[0:7]=="1101111":
+    instruction="jal"
+  j_type_implementation(instruction ,str[-12:-7:1], str[-20:-15:1], str[-25:-20:1])
 
 
 
